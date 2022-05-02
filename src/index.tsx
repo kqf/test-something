0cba3f7f66f3e0c5410d5bf64651b8834aa07169
kqf <q.dev.null@gmail.com>
2022-05-02 07:35:05 +0200

    Render squares (#5)
    * Add styles
    
    * Add the code
    
    * Render the numbers
    
    * Fix the typing

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-class Square extends React.Component {
+class Square extends React.Component<{ value: number }> {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
@@ -13,7 +13,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i: number) {
-    return <Square />;
+    return <Square value={i}/>;
   }
 
   render() {
