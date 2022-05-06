eb58b8fc75b09f3f0f9f3170f03c4de7765bf0af
Sasha <q.dev.null@gmail.com>
2022-05-06 16:18:54 +0200

    Add the main logic

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props: {value: string, onClick: () => void}) {
+function Square(props: { value: string, onClick: () => void }) {
   return (
     <button className="square" onClick={props.onClick}>
       {props.value}
@@ -74,6 +74,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares: Array<number>) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // @ts-ignore
