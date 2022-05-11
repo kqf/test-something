cdbc7ae058837d1645a6ebc06193df4a7b8b5f06
Sasha <q.dev.null@gmail.com>
2022-05-11 08:25:45 +0100

    Add the step numbers

--- src/index.tsx
+++ src/index.tsx
@@ -41,12 +41,13 @@ class Board extends React.Component<{ squares: Array<string>, onClick: (i: numbe
   }
 }
 
-class Game extends React.Component<{}, { history: Array<Array<string>>, xIsNext: boolean }> {
+class Game extends React.Component<{}, { history: Array<Array<string>>, xIsNext: boolean, stepNumber: number }> {
   constructor(props: {}) {
     super(props);
     this.state = {
       history: [Array(9).fill(null!)],
       xIsNext: true,
+      stepNumber: 0,
     };
   }
 
